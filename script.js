6ce9038e61e6ff5d1e5c13bc170a4ae2c4d2ab2d
jared@xtradite-digital.co.uk <jared@xtradite-digital.co.uk>
2025-07-20 10:25:53 +0100

    init-commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Place the JavaScript logic from the previous answer here
+// Due to length, it is expected to be split manually or inserted programmatically
+// Placeholder for now
+console.log("Quiz JS loaded");
+// Add logic as per previous quiz script
